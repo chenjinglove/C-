28a44c67e425de65c726bf69f2a3df3206795f64
chenjinglove <49227756+chenjinglove@users.noreply.github.com>
2019-11-02 19:11:45 +0800

    第一次提交文件监控系统代码

---
+++ FileMonitoringSystem/文件监控系统/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace 文件监控系统
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tvFolders = new System.Windows.Forms.TreeView();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiPaster = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtFilePath);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Location = new System.Drawing.Point(1, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(762, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "条件筛选";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Controls.Add(this.panel1);
+            this.groupBox2.Location = new System.Drawing.Point(1, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(762, 387);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件管理";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(28, 20);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(141, 20);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(349, 21);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tvFolders);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(3, 17);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 367);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lvFiles);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(203, 17);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(556, 367);
+            this.panel2.TabIndex = 1;
+            // 
+            // tvFolders
+            // 
+            this.tvFolders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tvFolders.ContextMenuStrip = this.contextMenuStrip2;
+            this.tvFolders.Location = new System.Drawing.Point(0, 3);
+            this.tvFolders.Name = "tvFolders";
+            this.tvFolders.Size = new System.Drawing.Size(200, 361);
+            this.tvFolders.TabIndex = 0;
+            this.tvFolders.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvFolders_AfterSelect);
+            this.tvFolders.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tvFolders_MouseDown);
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chType,
+            this.chPath,
+            this.chSize});
+            this.lvFiles.ContextMenuStrip = this.contextMenuStrip1;
+            this.lvFiles.FullRowSelect = true;
+            this.lvFiles.HideSelection = false;
+            this.lvFiles.Location = new System.Drawing.Point(0, 3);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(553, 361);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.SelectedIndexChanged += new System.EventHandler(this.lvFiles_SelectedIndexChanged);
+            // 
+            // chName
+            // 
+            this.chName.Text = "文件名";
+            this.chName.Width = 94;
+            // 
+            // chType
+            // 
+            this.chType.Text = "文件类型";
+            this.chType.Width = 76;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "文件路径";
+            this.chPath.Width = 141;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "文件大小（kb）";
+            this.chSize.Width = 109;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.tsmiCopy,
+            this.tsmiCut,
+            this.tsmiDelete});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 92);
+            // 
+            // tsmiCopy
+            // 
+            this.tsmiCopy.Name = "tsmiCopy";
+            this.tsmiCopy.Size = new System.Drawing.Size(100, 22);
+            this.tsmiCopy.Text = "复制";
+            this.tsmiCopy.Click += new System.EventHandler(this.tsmiCopy_Click);
+            // 
+            // tsmiCut
+            // 
+            this.tsmiCut.Name = "tsmiCut";
+            this.tsmiCut.Size = new System.Drawing.Size(100, 22);
+            this.tsmiCut.Text = "剪切";
+            this.tsmiCut.Click += new System.EventHandler(this.tsmiCut_Click);
+            // 
+            // tsmiDelete
+            // 
+            this.tsmiDelete.Name = "tsmiDelete";
+            this.tsmiDelete.Size = new System.Drawing.Size(100, 22);
+            this.tsmiDelete.Text = "删除";
+            this.tsmiDelete.Click += new System.EventHandler(this.tsmiDelete_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiPaster});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(101, 26);
+            // 
+            // tsmiPaster
+            // 
+            this.tsmiPaster.Name = "tsmiPaster";
+            this.tsmiPaster.Size = new System.Drawing.Size(100, 22);
+            this.tsmiPaster.Text = "粘贴";
+            this.tsmiPaster.Click += new System.EventHandler(this.tsmiPaster_Click);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(100, 22);
+            this.tsmiOpen.Text = "打开";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(532, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始监控";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(646, 18);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停止监控";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 469);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "文件磁盘管理";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.ColumnHeader chPath;
+        private System.Windows.Forms.ColumnHeader chSize;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView tvFolders;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopy;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCut;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDelete;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPaster;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FileMonitoringSystem/文件监控系统/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace 文件监控系统
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FolderBrowserDialog fbd;
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            DialogResult result =  fbd.ShowDialog();//打开模式窗体  线程停止在这个地方
+            if(result==DialogResult.OK)
+            {
+                txtFilePath.Text= fbd.SelectedPath;
+                //先把指定路径磁盘路径读取
+
+                TreeNode rootnode = new TreeNode(); //创建根节点
+                string nodeName = this.txtFilePath.Text;
+                rootnode.Text = nodeName.Substring(nodeName.LastIndexOf('\\')+1);//字符串截取
+                rootnode.Tag = this.txtFilePath.Text;//完整路径
+
+                this.tvFolders.Nodes.Add(rootnode);    //添加文件夹目录
+
+                //循环递归读取
+                loadFolder(rootnode);
+            }
+            //this.tvFolders.ExpandAll();//展开所有的树节点
+        }
+        /// <summary>
+        /// 递归加载文件夹
+        /// </summary>
+        /// <param name="childnode"></param>
+        private void loadFolder(TreeNode childnode)
+        {
+            try
+            {
+                //Directory 静态类 提供物理文件操作方法
+                DirectoryInfo directoryInfo = new DirectoryInfo(childnode.Tag.ToString());//非静态类 提供读取文件详细信息
+                DirectoryInfo[] list = directoryInfo.GetDirectories();
+
+                foreach (DirectoryInfo di in list)
+                {
+                    TreeNode tn = new TreeNode();
+                    tn.Text = di.Name;
+                    tn.Tag = di.FullName;//全路径
+                    childnode.Nodes.Add(tn);//添加到上一级节点
+                    loadFolder(tn); //循环调用
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        /// <summary>
+        /// 节点发生改变是更新
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvFolders_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.lvFiles.Items.Clear();  //清除之前选中项
+            TreeNode selectNode = this.tvFolders.SelectedNode; //选中节点
+            loadFiles(selectNode.Tag.ToString()); //获取文件夹的全路径
+        }
+        /// <summary>
+        /// 加载物理文件信息
+        /// </summary>
+        /// <param name="path"></param>
+        private void loadFiles(string path)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                FileInfo[] fileInfos = directoryInfo.GetFiles();//获取当前文件的所有文件
+                foreach (FileInfo fileInfo in fileInfos)
+                {
+                    ListViewItem lvi = new ListViewItem(fileInfo.Name);//主文本项
+                    long size = fileInfo.Length > 1024 ? fileInfo.Length / 1024 : 1;
+                    lvi.SubItems.AddRange(new string[] {
+                    fileInfo.Extension,
+                    fileInfo.FullName,
+                    size.ToString()
+                });
+                    lvi.Tag = fileInfo;//附加值绑定对象
+                    this.lvFiles.Items.Add(lvi);
+                    //添加到父容器
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+               
+            
+        }
+
+        private void tvFolders_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Right)
+            {
+                TreeNode tn = this.tvFolders.GetNodeAt(e.X, e.Y);
+                if(tn!=null)
+                {
+                    this.tvFolders.SelectedNode = tn;
+                }
+            }
+        }
+
+        private ListViewItem selectItem;
+        /// <summary>
+        /// 列表控件显示事件(选中)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lvFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(this.lvFiles.SelectedItems.Count>0)
+            selectItem = this.lvFiles.SelectedItems[0];//默认选中下标0 即第一个
+        }
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            //文件打开分两种 一种是通过文件流操作文件内容
+            //另一种是打开进程  后缀名识别
+            string filepath = selectItem.SubItems[2].Text;
+            Process.Start(filepath);
+        }
+        /// <summary>
+        /// 删除事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result= MessageBox.Show("确定要删除？","提升",MessageBoxButtons.YesNo);
+            if (result== DialogResult.Yes)
+            { 
+                File.Delete(selectItem.SubItems[2].Text);//不可逆转删除
+                selectItem = null;//清除对象
+                this.lvFiles.SelectedItems[0].Remove();//移除对象
+            }
+        }
+
+        private string filePath;//保存复制或剪切路径
+        private bool isDelete = false;// 复制:true 剪切：false
+        /// <summary>
+        /// 复制事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiCopy_Click(object sender, EventArgs e)
+        {
+            filePath = selectItem.SubItems[2].Text;
+            isDelete = true;
+        }
+        /// <summary>
+        /// 剪切事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiCut_Click(object sender, EventArgs e)
+        {
+            filePath = selectItem.SubItems[2].Text;
+            isDelete = false;
+        }
+        /// <summary>
+        /// 粘贴事件    
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiPaster_Click(object sender, EventArgs e)
+        {
+            #region 物理文件操作
+            //获取文件的保存路径
+            string newPath = Path.Combine( this.tvFolders.SelectedNode.Tag.ToString(),selectItem.Text);
+            if(isDelete)
+            {
+                File.Copy(filePath,newPath,true);//复制文件，且如果同名，进行覆盖 
+            }
+            else
+            {
+                File.Move(filePath,newPath);
+            }
+            #endregion
+            #region 界面数据操作
+            FileInfo fileInfo = new FileInfo(newPath);
+            ListViewItem lvi = new ListViewItem(fileInfo.Name);//主文本项
+            long size = fileInfo.Length > 1024 ? fileInfo.Length / 1024 : 1;
+            lvi.SubItems.AddRange(new string[] {
+                    fileInfo.Extension,
+                    fileInfo.FullName,
+                    size.ToString()
+            });
+            this.lvFiles.Items.Add(lvi);
+            #endregion
+        }
+
+        FileSystemWatcher fsw;
+        /// <summary>
+        /// 开始监控事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            fsw = new FileSystemWatcher();
+            //设置监控路径
+            try
+            {
+                fsw.Path = fbd.SelectedPath;
+                fsw.IncludeSubdirectories = true;//设置是否包含子文件夹
+                fsw.NotifyFilter = NotifyFilters.Size | NotifyFilters.FileName;
+                fsw.Filter = "*";
+                fsw.Changed += Fsw_Changed;//通讯协议
+                fsw.EnableRaisingEvents = true;//设置控件启用
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请先选择文件！！！");
+            }
+
+        }
+        /// <summary>
+        /// 触发事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            FileInfo fileInfo = selectItem.Tag as FileInfo;
+            MessageBox.Show("文件名：" + fileInfo);
+        }
+
+        /// <summary>
+        /// 关闭监控事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("未发现异常，停止监控!");
+        }
+    }
+}
